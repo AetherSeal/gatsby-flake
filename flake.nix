20aedf6d4e6d4854db5232b7185cb3b3d86eebd4
Alejandro Castro <alejandro.castro.q@gmail.com>
2024-11-29 20:32:39 -0300

    flake updated

--- flake.nix
+++ flake.nix
@@ -1,80 +1,41 @@
 {
-  description = "A Nix-flake-based Node | Gatsby development environment";
-
-  # GitHub URLs for the Nix inputs we're using
-  inputs = {
-    # Simply the greatest package repository on the planet
-    nixpkgs.url = "github:NixOS/nixpkgs";
-    # A set of helper functions for using flakes
-    flake-utils.url = "github:numtide/flake-utils";
-  };
-
-  outputs = { self, nixpkgs, flake-utils }:
+    description = "A Nix-flake-based Node | Gatsby development environment";
+    
+    # GitHub URLs for the Nix inputs we're using
+    inputs = {
+        # Simply the greatest package repository on the planet
+        nixpkgs.url = "github:NixOS/nixpkgs";
+        # A set of helper functions for using flakes
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+    
+    outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
+        let
         pkgs = import nixpkgs { inherit system; };
-
-        zsh = pkgs.zsh;
+        
         node = pkgs.nodejs_latest;
         gatsby = pkgs.nodePackages_latest.gatsby-cli;
         pnpm = pkgs.pnpm;
-        ohMyZsh = pkgs.oh-my-zsh;
-        powerlevel10k = pkgs.zsh-powerlevel10k;
-
-      in {
-        devShells = {
-          
-          default = pkgs.mkShell {
-            # Packages included in the environment
-            buildInputs = [ zsh node gatsby pnpm ohMyZsh powerlevel10k];
-
-            # Run when the shell is started up
-            shellHook = ''
-              echo "=> Resume ---------------------------------------------"
-              echo "zsh => `${zsh}/bin/zsh --version`"
-              echo "node => `${node}/bin/node --version`"
-              echo "gatsby-cli => `${gatsby}/bin/gatsby --version`"
-              echo "pnpm => `${pnpm}/bin/pnpm --version`"
-              echo "-------------------------------------------------------"
-              export SHELL=$(which zsh)
-              
-              echo "Welcome to the development environment that uses Zsh and avoids the config prompt!";
-
-              # Create a .zshrc file to avoid interactive configuration
-              if [ ! -f ~/.zshrc ]; then
-                echo "Creating .zshrc file..."
-                cat > ~/.zshrc <<EOF
-                # Zsh configurations
-                export ZSH=\$HOME/.oh-my-zsh
-                export ZSH_THEME="powerlevel10k/powerlevel10k"
-                plugins=(git)
-                source \$ZSH/oh-my-zsh.sh
-                EOF
-              fi
-              # Ensure we're using Zsh
-              if [ -z "$ZSH_VERSION" ]; then
-                echo "Not using Zsh. Switching to Zsh..."
-                exec zsh
-              fi
-
-              # Set up Oh My Zsh
-              export ZSH="$HOME/.oh-my-zsh"
-              source $ZSH/oh-my-zsh.sh
-
-              # Load Powerlevel10k theme
-              source $ZSH/custom/themes/powerlevel10k/powerlevel10k.zsh-theme
-              
-              # Config git
-              git config --global user.name "Alejandro Castro"
-              git config --global user.email "alejandro.castro.q@gmail.com"
-
-              echo "Done!"
-
-            '';
-
-          };
-        };
-      }
+        
+        in {
+            devShells = {
+                
+                default = pkgs.mkShell {
+                    # Packages included in the environment
+                    buildInputs = [ node gatsby pnpm ];
+                    
+                    # Run when the shell is started up
+                    shellHook = ''
+                    echo "=> Resume ---------------------------------------------"
+                    echo "node => `${node}/bin/node --version`"
+                    echo "gatsby-cli => `${gatsby}/bin/gatsby --version`"
+                    echo "pnpm => `${pnpm}/bin/pnpm --version`"
+                    echo "-------------------------------------------------------"
+                    '';                    
+                };
+            };
+        }
     );
 }
 
--- linuxEnv.md
+++ linuxEnv.md
@@ -5,27 +5,51 @@
 Go to the microsoft store and download the debian image
 
 ```bash
-# install curl wget & xz-utils
+# update system
+sudo apt update
+# install curl wget xz-utils & git
 sudo apt install wget curl xz-utils git
+```
+
+## 🧑‍💻Terminal (optional)
+
+Install oh-my-bash
+
+```bash
+# download and run oh-my-bash install script
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+# substitute the theme name in ~/.bashrc
+sed -i 's/OSH_THEME=".*"/OSH_THEME="powerbash10k"/' ~/.bashrc
+# reloads the .bashrc file
+source ~/.bashrc
+```
+
+Configure Git
+
+```bash
+brew install git
+git config --global user.name "Alejandro Castro"
+git config --global user.email "Alejandro Castro"
 ```
 
 ## 📦 Enviroment
 
-Follow the commands
+Follow the commands in order to install and configure nix
 
 ```bash
 # install nix
 sh <(curl -L https://nixos.org/nix/install) --daemon
 # modify nix.conf to enable nix flake
 echo "experimental-features = nix-command flakes" | sudo tee -a /etc/nix/nix.conf
-# use my flake
-
 ```
 
-- install and configure git
+Basic commands of nix flakes
 
 ```bash
-brew install git
-git config --global user.name "Alejandro Castro"
-git config --global user.email "Alejandro Castro"
+# Updates all inputs of the flake to their latest versions.
+nix flake update
+# Runs an app from a remote flake without installing it.
+nix run github:user/repo
+# Opens a development environment from a remote flake.
+nix develop github:user/repo
 ```
